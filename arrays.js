61b4c9b9894c74f6231196e783f779a756a39bca
Nivetha <nivedha.orton@gmail.com>
2019-08-12 06:12:24 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -27,6 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
   return array.shift()
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  return array.slice(1);
+  array.slice(1);
+  return array
 
 }
