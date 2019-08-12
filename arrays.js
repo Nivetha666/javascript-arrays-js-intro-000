55227584e93a2dedffd627120d12e5ddc793c174
Nivetha <nivedha.orton@gmail.com>
2019-08-12 05:30:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,4 +3,5 @@ var chocolateBars = ["snickers","hundredgrand","kitkat","skittles"];
 function addElementToBeginningOfArray(array,element){
   array.unshift(element);
   console.log(array);
+  return;
 }
